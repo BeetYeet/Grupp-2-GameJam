fb7e4a9b808200cd9f1cb2270f0650e9a5b2767e
William Hagström <hagwill@hotmail.se>
2019-01-23 17:25:38 +0100

    Movement
    Fix the max output is greater than the joystick range issue

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [Range(1,20)]
+    public float movementSpeed;
+
+    Vector2 movement;
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal") * movementSpeed; movement.y = Input.GetAxis("Vertical") * movementSpeed;
+        print(movement/movementSpeed);
+        rb.velocity = movement;
+    }
+}
