2f92e17462c7d82538ae1340e10120a0af30db0e
Alfred E <alfred.engstromer@elev.ga.lbs.se>
2019-01-24 00:48:10 +0100

    Made cool patterns possible

--- Assets/Scripts/BaseShooting.cs
+++ Assets/Scripts/BaseShooting.cs
@@ -4,39 +4,65 @@ using UnityEngine;
 
 public class BaseShooting : MonoBehaviour
 {
-
-    [Header("Atrubutes")]
-    public float projectileSpeed;
-    public float firerate;
-    private float fireCooldown = 0.1f;
+	public bool playerControllable = false;
 	public Faction.Side side;
 
 	[Header("Linkings")]
-    public GameObject projectile;
-    public Transform spawnPoint;
+	public BulletPattern bp;
+	public GameObject bullet;
+	public Transform spawnPoint;
 
-    // Start is called before the first frame update
-    void Start()
+	// Start is called before the first frame update
+	void Start()
     {
-        
+		ComplexPattern _ = new ComplexPattern();
+		_.actions = new Queue<BulletActionNode>();
+		
+		_.actions.Enqueue( new Fire( -10f ) );
+		_.actions.Enqueue( new Delay( 0.2f ) );
+		_.actions.Enqueue( new Fire( -5f ) );
+		_.actions.Enqueue( new Delay( 0.2f ) );
+		_.actions.Enqueue( new Fire( 0f ) );
+		_.actions.Enqueue( new Delay( 0.2f ) );
+		_.actions.Enqueue( new Fire( 5f ) );
+		_.actions.Enqueue( new Delay( 0.2f ) );
+		_.actions.Enqueue( new Fire( 10f ) );
+		_.actions.Enqueue( new Delay( 0.2f ) );
+		_.actions.Enqueue( new Fire( 5f ) );
+		_.actions.Enqueue( new Delay( 0.2f ) );
+		_.actions.Enqueue( new Fire( 0f ) );
+		_.actions.Enqueue( new Delay( 0.2f ) );
+		_.actions.Enqueue( new Fire( -5f ) );
+		_.actions.Enqueue( new Delay( 0.2f ) );
+		_.actions.Enqueue( new Fire( -10f ) );
+		_.actions.Enqueue( new Delay( 0.2f ) );
+		
+
+		/*
+		_.actions.Enqueue( new Fire( -10f ) );
+		_.actions.Enqueue( new Fire( 0f ) );
+		_.actions.Enqueue( new Fire( 10f ) );
+		_.actions.Enqueue( new Delay( 0.5f ) );
+		_.actions.Enqueue( new Fire( -7f ) );
+		_.actions.Enqueue( new Fire( 7f ) );
+		_.actions.Enqueue( new Delay( 0.5f ) );
+		*/
+		_.projectileSpeed = 0.05f;
+		bp = _;
+		bp.spawnPoint = spawnPoint;
+		bp.bullet = bullet;
+		bp.side = side;
     }
 
     // Update is called once per frame
     void Update()
     {
-        fireCooldown -= Time.deltaTime;
-        if(Input.GetButton("Fire1"))
+        if(!Input.GetButton("Fire1") && playerControllable)
         {
-            Shooter();
-        }
+			return;
+		}
+		bp.Update();
     }
 
-    void Shooter()
-    {
-
-        GameObject newProjectile = Instantiate(projectile, spawnPoint.position, spawnPoint.rotation);
-		newProjectile.GetComponent<BulletMovement>().Initialize(projectileSpeed, side);
-        fireCooldown = firerate;
-
-    }
+	
 }
---
+++ Assets/Scripts/BulletPattern.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BulletPattern
+{
+	public float projectileSpeed = 1f;
+	public Faction.Side side;
+	internal float fireCooldown = 0f;
+	public GameObject bullet;
+	public Transform spawnPoint;
+
+	internal void Shoot( float direction )
+	{
+		Quaternion dir = Quaternion.identity * create_from_axis_angle( 0f, 0f, 1f, direction );
+		GameObject b = Object.Instantiate( bullet, spawnPoint.position, dir );
+		b.GetComponent<BulletMovement>().Initialize( projectileSpeed, side );
+	}
+	public abstract void Update();
+
+	Quaternion create_from_axis_angle( float xx, float yy, float zz, float a )
+	{
+		a *= Mathf.PI / 180;
+		// Here we calculate the sin( theta / 2) once for optimization
+		float factor = Mathf.Sin( a / 2.0f );
+
+		// Calculate the x, y and z of the quaternion
+		float x = xx * factor;
+		float y = yy * factor;
+		float z = zz * factor;
+
+		// Calcualte the w value by cos( theta / 2 )
+		float w = Mathf.Cos( a / 2.0f );
+
+		return new Quaternion( x, y, z, w );
+	}
+}
+
+public class BasicPattern: BulletPattern
+{
+	public float firerate;
+
+	public BasicPattern( float firerate )
+	{
+		this.firerate = firerate;
+	}
+
+	public override void Update()
+	{
+		fireCooldown -= Time.deltaTime;
+		if ( fireCooldown <= 0f )
+		{
+			Shoot( 0f );
+			fireCooldown += firerate;
+		}
+	}
+}
+
+public class ComplexPattern: BulletPattern
+{
+	public Queue<BulletActionNode> actions;
+
+	public override void Update()
+	{
+		fireCooldown -= Time.deltaTime;
+		if ( fireCooldown <= 0f )
+		{
+			Process();
+		}
+	}
+
+	void Process()
+	{
+		BulletActionNode action = actions.Dequeue();
+		action.Trigger( this );
+		actions.Enqueue( action );
+		if ( actions.Peek().GetType() != typeof( Delay ) )
+		{
+			Process();
+		}
+	}
+}
+
+
+public abstract class BulletActionNode
+{
+	public abstract void Trigger( BulletPattern parent );
+}
+public class Delay: BulletActionNode
+{
+	public float delayTime;
+
+	public Delay( float delayTime )
+	{
+		this.delayTime = delayTime;
+	}
+
+	public override void Trigger( BulletPattern parent )
+	{
+		parent.fireCooldown += delayTime;
+	}
+}
+public class Fire: BulletActionNode
+{
+	public float direction;
+
+	public Fire( float direction )
+	{
+		this.direction = direction;
+	}
+
+	public override void Trigger( BulletPattern parent )
+	{
+		parent.Shoot( direction );
+	}
+}
+public class FireRandom: BulletActionNode
+{
+	public float directionMin;
+	public float directionMax;
+
+	public FireRandom( float directionMin, float directionMax )
+	{
+		this.directionMin = directionMin;
+		this.directionMax = directionMax;
+	}
+
+	public override void Trigger( BulletPattern parent )
+	{
+		parent.Shoot( Random.Range( directionMin, directionMax ) );
+	}
+}
+
