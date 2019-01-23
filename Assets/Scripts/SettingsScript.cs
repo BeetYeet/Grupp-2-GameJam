452c1f61fa1a4b31d9f1115f0215e89e2d0b5be3
William.T <william.thompson@elev.ga.lbs.se>
2019-01-23 19:08:03 +0100

    Lagt in musik och sound effects assets och fixat menyn.

---
+++ Assets/Scripts/SettingsScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsScript : MonoBehaviour
+{
+    public static float masterVolume;
+    public Slider volumeSLider;
+    AudioSource _audio;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        volumeSLider = GameObject.Find("Slider").GetComponent<Slider>();
+        _audio = GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(_audio.volume);
+        masterVolume = volumeSLider.value;
+        _audio.volume = masterVolume;
+    }
+}
