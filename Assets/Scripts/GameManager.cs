110466ec2c382bf3dffeafd639188d6bbc9ca8a1
Ajdin Talic <ajdinizbosne@gmail.com>
2019-01-23 16:48:46 +0100

    Game manager checks the distance traveled and can be paused when pausing the game or when dialog is displayed

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager current;
+
+    public float DistanceTraveled;
+    public bool pauseTravel;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (current != null)
+        {
+            throw new System.Exception("There can't Be 2 Game Controllers, duh");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!pauseTravel)
+        {
+            DistanceTraveled += Time.deltaTime; 
+        }
+    }
+}
