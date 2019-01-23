181ab55b3c064932b74f0c6ad23c72ff5f38cb8c
Alfred E <alfred.engstromer@elev.ga.lbs.se>
2019-01-23 15:34:26 +0100

    Did bullet damage

--- Assets/Scripts/BulletMovement.cs
+++ Assets/Scripts/BulletMovement.cs
@@ -2,24 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletMovement : MonoBehaviour
+public class BulletMovement: MonoBehaviour
 {
 	float movementSpeed = 1f;
 	Faction.Side side;
 
-	public void Initialize(float movementSpeed, Faction.Side bulletSide)
+	public void Initialize( float movementSpeed, Faction.Side bulletSide )
 	{
 		this.movementSpeed = movementSpeed;
 		side = bulletSide;
 	}
 
 	void Update()
-    {
+	{
 		transform.Translate( transform.forward * movementSpeed );
-    }
+	}
 
 	private void OnCollisionEnter2D( Collision2D collision )
 	{
-		SendMessageUpwards("TakeDamage", (object) side);
+		SendMessageUpwards( "TakeDamage", new DamageInfo(side, ()=> { Destroy( gameObject ); } ) );
 	}
 }
---
+++ Assets/Scripts/DamageInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageInfo
+{
+	Faction.Side side;
+	Action callback;
+
+	public DamageInfo( Faction.Side side, Action callback )
+	{
+		this.side = side;
+		this.callback = callback;
+	}
+}
