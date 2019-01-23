364a4a5920c10b3c997dae9781f339479847f55f
William Hagström <hagwill@hotmail.se>
2019-01-23 16:45:40 +0100

    BackgroundScrollComplete

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement: MonoBehaviour
+{
+	float movementSpeed = 1f;
+	Faction.Side side;
+
+	public void Initialize( float movementSpeed, Faction.Side bulletSide )
+	{
+		this.movementSpeed = movementSpeed;
+		side = bulletSide;
+	}
+
+	void Update()
+	{
+		transform.Translate( transform.forward * movementSpeed );
+	}
+
+	private void OnCollisionEnter2D( Collision2D collision )
+	{
+		SendMessageUpwards( "TakeDamage", new DamageInfo(side, ()=> { Destroy( gameObject ); } ) );
+	}
+}
